f2d4fd60dedf5690af892220bf0c31c40f780908
real_sohel <salmanisohel26@gmail.com>
2023-05-06 00:41:09 +0530

    All done only animation remaining

--- src/components/Descriptions.jsx
+++ src/components/Descriptions.jsx
@@ -1,13 +1,74 @@
 import React from 'react'
 import "../assets/descriptions.css"
+import {FaArrowDown, FaArrowUp, FaWind} from 'react-icons/fa';
+import {BiHappy} from 'react-icons/bi';
+import {MdCompress, MdOutlineWaterDrop} from 'react-icons/md'
 
-const Descriptions = () => {
+const Descriptions = (props) => {
+
+    const tempUnit = props.units === 'metric'?'°C':'°F';
+    const windUnit = props.units === 'metric'?'m/s':'m/h';
+
+    const cards = [
+        {
+            id:1,
+            icon: <FaArrowDown />,
+            title:'Min',
+            data: props.weather.temp_min,
+            unit: tempUnit,
+        },
+        {
+            id:2,
+            icon: <FaArrowUp />,
+            title:'Max',
+            data: props.weather.temp_max,
+            unit: tempUnit,
+        },
+        {
+            id:3,
+            icon: <BiHappy />,
+            title:'feels like',
+            data: props.weather.feels_like,
+            unit: tempUnit,
+        },
+        {
+            id:4,
+            icon: <MdCompress />,
+            title:'pressure',
+            data: props.weather.pressure,
+            unit: "hPa",
+        },
+        
+        {
+            id:5,
+            icon: <MdOutlineWaterDrop />,
+            title:'humidity',
+            data: props.weather.humidity,
+            unit: "%",
+        },
+        
+        {
+            id:6,
+            icon: <FaWind />,
+            title:'wind speed',
+            data: props.weather.speed,
+            unit: windUnit,
+        },
+    ]
     return (
         <>
-        <div className="section sectioon__descriptions">
-            <div className="card">
-                <div className="description__card-icon"></div>
+        <div className="section sectioon__descriptions grid  grid-cols-3 gap-10 mt-20 text-lg font-medium">
+        {cards.map(({id,icon,title,data,unit}) =>(
+
+            <div key={id}className="card flex flex-col items-center justify-between p-4 rounded-[0.4rem]">
+                <div className="description__card-icon w-[100%] flex flex-row items-center justify-center gap-1 mt-2 mb-10 md:mb-4 ">
+                    {icon}
+                    <small className='capitalize'>{title}</small>
+                </div>
+                <h2>{`${data} ${unit}`}</h2>
             </div>
+        ))}
+            
         </div>
         </>
     )
---
+++ src/weatherService.js
@@ -0,0 +1,38 @@
+const API_KEY = "82cc218063ba0f63f528f0e8effd065f";
+
+const makeIcon = (icon)=> `https://openweathermap.org/img/wn/${icon}@2x.png`
+
+
+const getFormattedData= async(city, units= 'metric')=>{
+    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=${units}`;
+
+    const data = await fetch(URL)
+        .then((res)=>res.json()).then((data)=>data);
+    // console.log(data)
+    const{
+        weather,
+        main:{
+            temp_min, temp_max, feels_like, humidity, temp, pressure},
+        sys:{country},
+        wind:{speed},
+        name,
+    } = data;
+
+    const {description, icon } = weather[0];
+    
+    return {
+        description,
+        iconURL: makeIcon(icon),
+        temp,
+        feels_like,
+        temp_max,
+        temp_min,
+        pressure,
+        humidity,
+        speed,
+        country,
+        name,
+    }
+}
+
+export  {getFormattedData};
